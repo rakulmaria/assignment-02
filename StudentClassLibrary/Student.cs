cefdcbb8a3037d390735f18949adfc10ba13563b
emjakobsen1 <erja@itu.dk>
2022-09-17 14:06:30 +0200

    A bit of exercise 1

---
+++ StudentClassLibrary/Student.cs
@@ -0,0 +1,61 @@
+﻿namespace StudentClassLibrary;
+
+public class Student
+{
+    public int Id { get; init; }
+
+    public Student(
+        int id,
+        string givenName,
+        string surname,
+        DateTime startDate,
+        DateTime endDate,
+        DateTime graduationDate
+    )
+    {
+        this.Id = id;
+        this.GivenName = givenName;
+        this.Surname = surname;
+        this.StartDate = startDate;
+        this.EndDate = endDate;
+        this.GraduationDate = graduationDate;
+    }
+
+    public string GivenName { get; set; }
+
+    public string Surname { get; set; }
+
+    public enum _Status
+    {
+        New,
+        Active,
+        Dropout,
+        Graduated
+    };
+
+    public _Status Status { get; }
+
+    public DateTime StartDate { get; set; }
+
+    public DateTime EndDate { get; set; }
+
+    public DateTime GraduationDate { get; set; }
+
+    public override string ToString()
+    {
+        return "Id: "
+            + Id
+            + ", Givenname: "
+            + GivenName
+            + ", Surname: "
+            + Surname
+            + " Status: "
+            + Status
+            + "Start date: "
+            + StartDate
+            + ", End date: "
+            + EndDate
+            + ", Graduation Date: "
+            + GraduationDate;
+    }
+}
