660b37b09ea4adb74104261b0a0b3a878ab0dcab
emjakobsen1 <erja@itu.dk>
2022-09-18 16:01:33 +0200

    done

--- Assignment2.Tests/QueriesTests.cs
+++ Assignment2.Tests/QueriesTests.cs
@@ -3,8 +3,61 @@ namespace Assignment2.Tests;
 public class QueriesTests
 {
     [Fact]
-    public void Test1()
+    public void WizardCollection_With_Rowling()
     {
-
+        var wizards = WizardCollection.Create();
+        var expected = new List<string> {"Harry Potter","Ron","Hermione","Dumbledore"};
+        Assert.Equal(expected, Queries.QueryOne(wizards));
+        
     }
+    [Fact]
+    public void WizardCollection_With_Rowling2()
+    {
+        var wizards = WizardCollection.Create();
+        var expected = new List<string> {"Harry Potter","Ron","Hermione","Dumbledore"};
+        Assert.Equal(expected, Queries.QueryOneExtensionMethod(wizards));
+        
+    }
+
+    [Fact]
+    public void WizardCollection_With_Darth()
+    {
+        var wizards = WizardCollection.Create();
+        var expected = 1977;
+        Assert.Equal(expected, Queries.QueryTwo(wizards));
+        
+    }
+     [Fact]
+    public void WizardCollection_With_Darth2()
+    {
+        var wizards = WizardCollection.Create();
+        var expected = 1977;
+        Assert.Equal(expected, Queries.QueryTwoExtensionMethod(wizards));
+        
+    }
+     [Fact]
+    public void WizardCollection_With_Unique_Wizards()
+    {
+        var wizards = WizardCollection.Create();
+        var expected = new List<(string,int)> {("Harry Potter",2020),("Ron",2020),("Hermione",2020),("Dumbledore",2020)};
+        Assert.Equal(expected, Queries.QueryThree(wizards));
+        
+    }
+     [Fact]
+    public void WizardCollection_With_Unique_Wizards2()
+    {
+        var wizards = WizardCollection.Create();
+        var expected = new List<(string,int)> {("Harry Potter",2020),("Ron",2020),("Hermione",2020),("Dumbledore",2020)};
+        Assert.Equal(expected, Queries.QueryThreeExtensionMethod(wizards));
+        
+    }
+    [Fact]
+    public void WizardCollection_With_Reverse_Order()
+    {
+        var wizards = WizardCollection.Create();
+        var expected = new List<string> {"J.K Rowling Harry Potter", "J.K Rowling Ron", "J.K Rowling Hermione"};
+        Assert.NotEqual(expected, Queries.QueryFourExtensionMethod(wizards));
+        
+    }
+
 }
--- Assignment2/Queries.cs
+++ Assignment2/Queries.cs
@@ -1,5 +1,73 @@
 namespace Assignment2;
+using System.Linq;
 
 public class Queries
 {
-}
+    public static IEnumerable<string> QueryOne(WizardCollection wizardCollection)
+    {
+        var rowling =
+            from w in wizardCollection
+            where w.Creator.Contains("Rowling")
+            select w.Name;
+        
+        rowling.ToList();
+        return rowling;    
+    }
+    public static IEnumerable<string> QueryOneExtensionMethod(WizardCollection wizardcollection){
+        return wizardcollection.Where(w => w.Creator.Contains("Rowling")).Select(w => w.Name).ToList();
+    }
+
+    public static int QueryTwo(WizardCollection wizardcollection){
+        var siths = 
+        from w in wizardcollection
+        where w.Name.Contains("Darth") 
+        orderby w.Year descending
+        select w.Year;
+        int firstyear = Convert.ToInt32(siths.First());
+        return firstyear; 
+    }
+    public static int QueryTwoExtensionMethod(WizardCollection wizardcollection){
+        var siths = wizardcollection.Where(w => w.Name.Contains("Darth")).OrderByDescending(w => w.Year).Select(w => w.Year);
+        int firstyear = Convert.ToInt32(siths.First());
+        return firstyear; 
+    }
+
+    public static IEnumerable<(string,int)> QueryThree(WizardCollection wizardcollection){
+       var uniquelist = from w in wizardcollection
+       where w.Medium.Contains("Harry Potter")
+       group w by w.Name into newGroup select newGroup.First();
+       
+       uniquelist.ToList();
+       foreach (Wizard w in uniquelist){
+            yield return (w.Name,Convert.ToInt32(w.Year));
+        }
+        
+
+    }
+    public static IEnumerable<(string,int)> QueryThreeExtensionMethod(WizardCollection wizardcollection){
+        var uniquelist = wizardcollection.Where(w => w.Medium.Contains("Harry Potter")).GroupBy(w => w.Name).Select(g => g.First()).ToList();
+        foreach (Wizard w in uniquelist){
+            yield return (w.Name,Convert.ToInt32(w.Year));
+        }
+    }
+    //List of wizard names grouped by creator in reverse order by creator name and then wizard name.
+    public static IEnumerable<string> QueryFour(WizardCollection wizardcollection){
+        var reverseOrder = from w in wizardcollection
+        group w by w.Creator into newGroup select newGroup.Reverse();
+        reverseOrder.ToList();
+        foreach (var w in reverseOrder){
+            foreach (var x in w){
+                yield return x.Creator + " "+ x.Name; 
+            }
+        }
+    }
+    public static IEnumerable<string> QueryFourExtensionMethod(WizardCollection wizardcollection){
+        var reverseOrder = wizardcollection.GroupBy(w => w.Creator).Select(group => group.Reverse().ToList());
+            foreach (var w in reverseOrder){
+            foreach (var x in w){
+                yield return x.Creator + " "+ x.Name; 
+            }
+        }
+        }
+    }
+
