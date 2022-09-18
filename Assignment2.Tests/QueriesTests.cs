1159042ea0d551e0e727f9bb261291c16a5f6053
gues <gues@itu.dk>
2022-09-18 16:16:02 +0200

    Implemented all queries and tests

--- Assignment2.Tests/QueriesTests.cs
+++ Assignment2.Tests/QueriesTests.cs
@@ -2,9 +2,122 @@ namespace Assignment2.Tests;
 
 public class QueriesTests
 {
-    [Fact]
-    public void Test1()
-    {
+    IEnumerable<Wizard> wizardCollection = WizardCollection.Create();
 
+    [Fact]
+    public void GetRowlingWizards_Given_Stream_Of_Wizards_Returns_Names_Of_Wizards_By_Rowling()
+    {
+        // Arrange
+        var expected = new[]
+        {
+            "Harry Potter",
+            "Bellatrix Lestrange",
+            "Sirius Black",
+            "Remus Lupin",
+            "Hermione Granger",
+            "Lord Voldemort",
+            "Professor McGonagall",
+            "Professor Snape",
+            "Professor Dumbledore",
+            "Mad-Eye Moody",
+        };
+
+        // Act
+        var actualLINQ = Queries.GetRowlingWizardsOne(wizardCollection);
+        var actualExtension = Queries.GetRowlingWizardsTwo(wizardCollection);
+
+        // Assert
+        Assert.Equal(expected, actualLINQ);
+        Assert.Equal(expected, actualExtension);
+    }
+
+    [Fact]
+    public void GetFirstSithLordYear_Given_Stream_Of_Wizards_Returns_Year_Of_First_Sith_Lord()
+    {
+        // Arrange
+        var expected = 1977;
+
+        // Act
+        var actualLINQ = Queries.GetFirstSithLordYearOne(wizardCollection);
+        var actualExtension = Queries.GetFirstSithLordYearTwo(wizardCollection);
+
+        // Assert
+        Assert.Equal(expected, actualLINQ);
+        Assert.Equal(expected, actualExtension);
+    }
+
+    [Fact]
+    public void GetUniqueRowlingWizards_Given_Stream_Of_Wizards_Returns_Name_And_Year_Of_Unique_Wizards()
+    {
+        // Arrange
+        IEnumerable<(string, int?)> expected = new List<(string, int?)>()
+        {
+            ("Harry Potter", 1997),
+            ("Bellatrix Lestrange", 1997),
+            ("Sirius Black", 1997),
+            ("Remus Lupin", 1997),
+            ("Hermione Granger", 1997),
+            ("Lord Voldemort", 1997),
+            ("Professor McGonagall", 1997),
+            ("Professor Snape", 1997),
+            ("Professor Dumbledore", 1997),
+            ("Mad-Eye Moody", 1997),
+        };
+
+        // Act
+        var actualLINQ = Queries.GetUniqueRowlingWizardsOne(wizardCollection);
+        var actualExtension = Queries.GetUniqueRowlingWizardsTwo(wizardCollection);
+
+        // Assert
+        Assert.Equal(expected, actualLINQ);
+        Assert.Equal(expected, actualExtension);
+    }
+
+    [Fact]
+    public void GetWizardsByCreator_Given_Stream_Of_Wizards_Returns_Wizards_By_Creator_Reversed()
+    {
+        // Arrange
+        IEnumerable<(string, IEnumerable<string>)> expected = new List<(string, IEnumerable<string>)>()
+        {
+            ("T.A. Barron", new[]
+            {
+                "Merlin",
+            }),
+            ("Stephen King", new[]
+            {
+                "Carrie",
+            }),
+            ("J.R.R. Tolkien", new[]
+            {
+                "Sauron",
+                "Gandalf",
+            }),
+            ("J.K. Rowling", new[]
+            {
+                "Harry Potter",
+                "Bellatrix Lestrange",
+                "Sirius Black",
+                "Remus Lupin",
+                "Hermione Granger",
+                "Lord Voldemort",
+                "Professor McGonagall",
+                "Professor Snape",
+                "Professor Dumbledore",
+                "Mad-Eye Moody",
+            }),
+            ("George Lucas", new[]
+            {
+                "Darth Vader",
+            }),
+        };
+
+        // Act
+        var actualLINQ = Queries.GetWizardsByCreatorOne(wizardCollection);
+        
+        var actualExtension = Queries.GetWizardsByCreatorTwo(wizardCollection);
+
+        // Assert
+        Assert.Equal(expected, actualLINQ);
+        Assert.Equal(expected, actualExtension);
     }
 }
--- Assignment2/Queries.cs
+++ Assignment2/Queries.cs
@@ -2,4 +2,58 @@ namespace Assignment2;
 
 public class Queries
 {
+    // 1. Wizards invented by Rowling. Only return the names.
+    
+    // LINQ implementation
+    public static IEnumerable<string> GetRowlingWizardsOne(IEnumerable<Wizard> wizards) =>
+        from w in wizards
+        where w.Creator.Contains("J.K. Rowling")
+        select w.Name;
+
+    // Extension method implementation 
+    public static IEnumerable<string> GetRowlingWizardsTwo(IEnumerable<Wizard> wizards) => 
+        wizards.Where(w => w.Creator.Contains("J.K. Rowling")).Select(w => w.Name);
+
+
+
+    // 2. The year the first sith lord was introduced. Let's define a sith lord to be one named Darth something.
+
+    // LINQ implementation
+    public static int? GetFirstSithLordYearOne(IEnumerable<Wizard> wizards) => 
+        (from w in wizards
+        where w.Name.Contains("Darth")
+        select w).First().Year;
+
+    // Extension method implementation 
+    public static int? GetFirstSithLordYearTwo(IEnumerable<Wizard> wizards) => 
+        wizards.First(w => w.Name.Contains("Darth")).Year;
+
+
+
+    // 3. Unique list of wizards from the Harry Potter books - only return name and year (as a value tuple).
+
+    // LINQ implementation
+    public static IEnumerable<(string name, int? year)> GetUniqueRowlingWizardsOne(IEnumerable<Wizard> wizards) => 
+        (from w in wizards
+        where w.Medium.Equals("Harry Potter")
+        select (w.Name, w.Year)).Distinct();
+
+    // Extension method implementation 
+    public static IEnumerable<(string name, int? year)> GetUniqueRowlingWizardsTwo(IEnumerable<Wizard> wizards) => 
+        wizards.Where(w => w.Medium.Equals("Harry Potter")).Select(w => (w.Name, w.Year)).Distinct();
+
+
+
+    // 4. List of wizard names grouped by creator in reverse order by creator name and then wizard name.
+
+    // LINQ implementation
+    public static IEnumerable<(string, IEnumerable<string>)> GetWizardsByCreatorOne(IEnumerable<Wizard> wizards) => 
+        from w in wizards
+        group w by w.Creator into g
+        orderby g.Key descending
+        select (g.Key, (from x in g select x.Name));
+
+    // Extension method implementation 
+    public static IEnumerable<(string, IEnumerable<string>)> GetWizardsByCreatorTwo(IEnumerable<Wizard> wizards) => 
+        wizards.GroupBy(w => w.Creator).OrderByDescending(w => w.Key).Select(w => (w.Key, w.Select(x => x.Name)));
 }
