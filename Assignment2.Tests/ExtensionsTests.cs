23bc8433e9ac7d4b4a3ba2ec94f1702974ee771a
Gustav <gues@it.dk>
2022-09-20 13:19:01 +0200

    Added Two First Extension Methods

--- Assignment2.Tests/ExtensionsTests.cs
+++ Assignment2.Tests/ExtensionsTests.cs
@@ -3,8 +3,37 @@ namespace Assignment2.Tests;
 public class ExtensionsTests
 {
     [Fact]
-    public void Test1()
+    public void Flatten_Given_List_Of_List_Returns_One_Combined_List()
     {
+        // Arrange
+        IEnumerable<int>[] listOfLists =
+        {
+            new List<int>() { 1, 2, 3, 4 },
+            new List<int>() { 4, 5, 6, 7 },
+            new List<int>() { 6, 7, 8, 9 }
+        };
 
+        var expected = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        // Act
+        var actual= Extensions.Flatten(listOfLists);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Divisible_Given_Array_Returns_List_Of_Numbers_Greater_Than_47_And_Divisible_By_7()
+    {
+        // Arrange
+        var data = new[] {1, 2, 7, 3, 50, 51, 52, 70};
+
+        var expected = new[] {70};
+
+        // Act
+        var actual= Extensions.Divisible(data);
+
+        // Assert
+        Assert.Equal(expected, actual);
     }
 }
--- Assignment2/Extensions.cs
+++ Assignment2/Extensions.cs
@@ -2,4 +2,9 @@ namespace Assignment2;
 
 public static class Extensions
 {
+    public static IEnumerable<int> Flatten(IEnumerable<int>[] numbers) =>
+        numbers.SelectMany(x => x).Distinct();
+
+    public static IEnumerable<int> Divisible(int[] numbers) =>
+        numbers.Where(n => n % 7 == 0 && n > 47);
 }
